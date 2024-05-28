c126b2d7b10c5230bc43f93386a8aed3da3a144a
ArandaHidalgoLuis <Arandahidalgoluis@gmail.com>
2024-05-28 12:20:39 +0200

    Initial commit

---
+++ freemt/malagaEmt.py
@@ -0,0 +1,41 @@
+from bs4 import BeautifulSoup
+import requests
+
+def inicializar(numeroParada):
+
+    page = requests.get('https://www.emtmalaga.es/emt-mobile/informacionParada.html?codParada='+numeroParada)
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    liBuses = soup.find_all('ul', {'data-role': 'listview'})
+    print(liBuses)
+    return liBuses
+
+def devolverNBuses(liBuses):
+    bus_identifiers = []
+    for ul in liBuses:
+        a_tags = ul.find_all('a')
+        for a in a_tags:
+            bus_id = a.get('title')
+            if bus_id:
+                bus_identifiers.append(bus_id)
+    return bus_identifiers
+
+def devolverTBuses(liBuses):
+    bus_times = []
+    for ul in liBuses:
+        span_tags = ul.find_all('span', class_=lambda x: x and x.startswith('minutos'))
+        for span in span_tags:
+            time_text = span.get_text(strip=True)
+            if time_text:
+                bus_times.append(time_text)
+    return bus_times
+
+
+#if __name__ == "__main__":
+#    numeroParada = input("Escribe el número de parada que deseas consultar: ")
+#    liBuses = inicializar(numeroParada)
+#    bus_identifiers = devolverNBuses(liBuses)
+#    bus_times = devolverTBuses(liBuses)
+
+#   print("Identificadores de buses:", bus_identifiers)
+#    print("Tiempos de buses:", bus_times)
