e106ec917c9be07cf7672e2cb04cdcc65ce27e92
ArandaHidalgoLuis <Arandahidalgoluis@gmail.com>
2024-05-29 11:22:45 +0200

    cleaner

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-Prueba
+if __name__ == "__main__":
+    numeroParada = input("Escribe el número de parada que deseas consultar: ")
+    liBuses = inicializar(numeroParada)
+    bus_identifiers = devolverNBuses(liBuses)
+    bus_times = devolverTBuses(liBuses)
+
+   print("Identificadores de buses:", bus_identifiers)
+   print("Tiempos de buses:", bus_times)
--- freemt/malagaEmt.py
+++ freemt/malagaEmt.py
@@ -30,11 +30,3 @@ def devolverTBuses(liBuses):
     return bus_times
 
 
-#if __name__ == "__main__":
-#    numeroParada = input("Escribe el número de parada que deseas consultar: ")
-#    liBuses = inicializar(numeroParada)
-#    bus_identifiers = devolverNBuses(liBuses)
-#    bus_times = devolverTBuses(liBuses)
-
-#   print("Identificadores de buses:", bus_identifiers)
-#    print("Tiempos de buses:", bus_times)
